6415877aedbf56cc898c54cfa2b16db649c99d27
Shaa test <shvyrev.anton78@gmail.com>
2023-10-21 17:49:17 +0500

    First homework

---
+++ Worker/src/Worker.java
@@ -0,0 +1,29 @@
+public class Worker {
+    private OnTaskDoneListener callback;
+    private OnTaskErrorListener errorCallback;
+
+    public Worker(OnTaskDoneListener callback, OnTaskErrorListener errorCallback) {
+        this.callback = callback;
+        this.errorCallback = errorCallback;
+    }
+
+    public void start() {
+        for (int i = 1; i <= 100; i++) {
+            if(i == 33){
+                errorCallback.onError("Error occurred on task " + i);
+            }
+            callback.onDone("Task " + i + " is done");
+        }
+    }
+}
+
+@FunctionalInterface
+interface OnTaskDoneListener{
+    void onDone(String result);
+}
+
+@FunctionalInterface
+interface OnTaskErrorListener{
+    void onError(String result);
+}
+
